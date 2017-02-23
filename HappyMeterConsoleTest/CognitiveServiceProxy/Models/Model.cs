840601208c3ae89914be05b0334fb3db322f1881
imironica <ionut.mironica@gmail.com>
2017-02-23 07:05:10 +0200

    Query for emotions from all images from a folder / link

---
+++ HappyMeterConsoleTest/CognitiveServiceProxy/Models/Model.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveServiceProxy.Models
+{
+
+
+
+    public class FaceEmotion
+    {
+        [JsonProperty(PropertyName = "faceRectangle")]
+        public FaceRectangle FaceRectangle { get; set; }
+        [JsonProperty(PropertyName = "scores")]
+        public Score Scores { get; set; }
+    }
+
+
+
+    [JsonObject(Title = "Facerectangle")]
+    public class FaceRectangle
+    {
+        [JsonProperty(PropertyName = "height")]
+        public int Height { get; set; }
+        [JsonProperty(PropertyName = "left")]
+        public int Left { get; set; }
+        [JsonProperty(PropertyName = "top")]
+        public int Top { get; set; }
+        [JsonProperty(PropertyName = "width")]
+        public int Width { get; set; }
+    }
+
+    [JsonObject(Title = "Scores")]
+    public class Score
+    {
+        [JsonProperty(PropertyName = "anger")]
+        public float Anger { get; set; }
+        [JsonProperty(PropertyName = "contempt")]
+        public float Contempt { get; set; }
+        [JsonProperty(PropertyName = "disgust")]
+        public float Disgust { get; set; }
+        [JsonProperty(PropertyName = "fear")]
+        public float Fear { get; set; }
+        [JsonProperty(PropertyName = "happiness")]
+        public float Happiness { get; set; }
+        [JsonProperty(PropertyName = "neutral")]
+        public float Neutral { get; set; }
+        [JsonProperty(PropertyName = "sadness")]
+        public float Sadness { get; set; }
+        [JsonProperty(PropertyName = "surprise")]
+        public float Surprise { get; set; }
+
+    }
+
+
+
+
+    /*
+        public class Rootobject
+        {
+            public FaceEmotion[] FaceEmotions { get; set; }
+        }
+
+        public class FaceEmotion
+        {
+            [JsonProperty(PropertyName = "faceRectangle")]
+            public FaceRectangle FaceRectangle { get; set; }
+            [JsonProperty(PropertyName = "scores")]
+            public Score[] Scores { get; set; }
+        }
+
+        [JsonObject(Title = "Facerectangle")]
+        public class FaceRectangle
+        {
+            [JsonProperty(PropertyName = "height")]
+            public int Height { get; set; }
+            [JsonProperty(PropertyName = "left")]
+            public int Left { get; set; }
+            [JsonProperty(PropertyName = "top")]
+            public int Top { get; set; }
+            [JsonProperty(PropertyName = "width")]
+            public int Width { get; set; }
+        }
+
+        [JsonObject(Title = "Scores")]
+        public class Score
+        {
+            [JsonProperty(PropertyName = "anger")]
+            public float Anger { get; set; }
+            [JsonProperty(PropertyName = "contempt")]
+            public float Contempt { get; set; }
+            [JsonProperty(PropertyName = "disgust")]
+            public float Disgust { get; set; }
+            [JsonProperty(PropertyName = "fear")]
+            public float Fear { get; set; }
+            [JsonProperty(PropertyName = "happiness")]
+            public float Happiness { get; set; }
+            [JsonProperty(PropertyName = "neutral")]
+            public float Neutral { get; set; }
+            [JsonProperty(PropertyName = "sadness")]
+            public float Sadness { get; set; }
+            [JsonProperty(PropertyName = "surprise")]
+            public float Surprise { get; set; }
+
+        }*/
+
+}
--- HappyMeterConsoleTest/HappyMeterConsoleTest/Program.cs
+++ HappyMeterConsoleTest/HappyMeterConsoleTest/Program.cs
@@ -1,15 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CognitiveServiceProxy;
+using CognitiveServiceProxy.Models;
+using Newtonsoft.Json;
+using System;
+using System.Configuration;
+using System.IO;
 
-namespace HappyMeterConsoleTest
+namespace EndavaHappiness
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var option = string.Empty;
+            while (option != "0")
+            {
+                ShowOptions();
+                option = Console.ReadLine();
+                if (option == "0")
+                    return;
+                if (option == "1")
+                    ComputeEmotionsFromFolder();
+                if (option == "2")
+                    ComputeEmotionsFromLink();
+            }
         }
+
+        private static void ComputeEmotionsFromFolder()
+        {
+            var apiCognitiveEmotionLink = ConfigurationManager.AppSettings["ApiCognitiveEmotionLink"].ToString();
+            var apiContentType = ConfigurationManager.AppSettings["ApiContentType"].ToString();
+            var apiKeyName = ConfigurationManager.AppSettings["ApiKeyName"].ToString();
+            var apiKey = ConfigurationManager.AppSettings["ApiKey"].ToString();
+            var sourceFolder = ConfigurationManager.AppSettings["SourceFolder"].ToString();
+            var saveFolder = ConfigurationManager.AppSettings["SaveFolder"].ToString();
+            ServiceProxy proxy = new ServiceProxy(apiCognitiveEmotionLink, apiKeyName, apiKey);
+
+            if (!Directory.Exists(saveFolder))
+                Directory.CreateDirectory(saveFolder);
+
+            var files = Directory.GetFiles(sourceFolder);
+            foreach (var file in files)
+            {
+                Console.WriteLine(string.Format("Processing {0}", file));
+                byte[] imgdata = System.IO.File.ReadAllBytes(file);
+
+                var response = proxy.PostImageStream(imgdata);
+                //FaceEmotion[] account = JsonConvert.DeserializeObject<FaceEmotion[]>(response);
+                //var emotionStr = JsonConvert.SerializeObject(response);
+                var filename = Path.Combine(saveFolder, Path.GetFileName(file)) + ".txt";
+                File.WriteAllText(filename, response);
+            }
+
+        }
+
+        private static void ComputeEmotionsFromLink()
+        {
+            var apiCognitiveEmotionLink = ConfigurationManager.AppSettings["ApiCognitiveEmotionLink"].ToString();
+            var apiContentType = ConfigurationManager.AppSettings["ApiContentType"].ToString();
+            var apiKeyName = ConfigurationManager.AppSettings["ApiKeyName"].ToString();
+            var apiKey = ConfigurationManager.AppSettings["ApiKey"].ToString();
+            var imageLink = ConfigurationManager.AppSettings["ImageLink"].ToString();
+            var saveFolder = ConfigurationManager.AppSettings["SaveFolder"].ToString();
+            ServiceProxy proxy = new ServiceProxy(apiCognitiveEmotionLink, apiKeyName, apiKey);
+
+            Uri uri = new Uri(imageLink);
+            string file = System.IO.Path.GetFileName(uri.AbsolutePath);
+            Console.WriteLine(string.Format("Processing {0}", imageLink));
+
+            var jsonPost = "{ \"url\": \"" + imageLink  + "\" }";
+            var response = proxy.PostJson(jsonPost);
+            //FaceEmotion[] account = JsonConvert.DeserializeObject<FaceEmotion[]>(response);
+            //var emotionStr = JsonConvert.SerializeObject(response);
+            if (!Directory.Exists(saveFolder))
+                Directory.CreateDirectory(saveFolder);
+            var filename = Path.Combine(saveFolder, file) + ".txt"; 
+            File.WriteAllText(filename, response);
+        }
+
+        private static void ShowOptions()
+        {
+            Console.WriteLine("Options: ");
+            Console.WriteLine("1 - Compute emotions from folder: ");
+            Console.WriteLine("2 - Compute emotions from link: ");
+            Console.WriteLine("0 - Exit: ");
+        }
+
     }
 }
