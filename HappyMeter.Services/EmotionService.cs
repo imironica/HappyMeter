a681008f4259ffbdd8585a02843fa8ddecc025bb
imironica <ionut.mironica@gmail.com>
2017-02-24 10:31:09 +0200

    add MongoDb Repository

--- HappyMeter.Services/EmotionService.cs
+++ HappyMeter.Services/EmotionService.cs
@@ -1,8 +1,5 @@
 ﻿using HappyMeter.Models;
-using MongoDB.Driver;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
 
 namespace HappyMeter.Services
 {
@@ -10,30 +7,15 @@ namespace HappyMeter.Services
     {
         public void AddEmotion(InfoDTO infoDTO)
         {
-            string dbName = ConfigurationManager.AppSettings["db"].ToString();
-            string connectionString = ConfigurationManager.AppSettings["dbIp"].ToString();
-            var client = new MongoClient(connectionString);
-
-            //Use the MongoClient to access the server
-            var database = client.GetDatabase(dbName);
-            var collection = database.GetCollection<InfoDTO>("faces");
-            collection.InsertOne(infoDTO);
+            var repo = new MDRepository<InfoDTO>();
+            repo.InsertOne(infoDTO);
         }
 
         public List<InfoDTO> GetEmotionsPerCategory(string category)
         {
-            string db = ConfigurationManager.AppSettings["db"].ToString();
-            string connectionString = ConfigurationManager.AppSettings["dbIp"].ToString();
-            // Create a MongoClient object by using the connection string
-            var client = new MongoClient(connectionString);
-
-            //Use the MongoClient to access the server
-            var database = client.GetDatabase(db);
-            var collection = database.GetCollection<InfoDTO>("faces");
-            var values = collection.AsQueryable<InfoDTO>().ToList();
+            var repo = new MDRepository<InfoDTO>();
+            var values = repo.GetAllList();
             return values;
         }
-
-
     }
 }
